aa710fb9527ac08659e6961a6d26a9a34a6f56f9
Jussi Martikainen <jmmartik@gmail.com>
2014-12-20 10:47:25 +0200

    Init canvas and draw some balls in it

--- src/bepallod/core.cljs
+++ src/bepallod/core.cljs
@@ -2,4 +2,27 @@
 
 (enable-console-print!)
 
-(println "Hello world!")
+(def canvas (.getElementById js/document "canvas"))
+(def context (.getContext canvas "2d"))
+
+(defn balls []
+  (for [x (range 25 425 50)
+        y (range 25 425 50)]
+    [x y]))
+
+(defn drawBall [x y]
+  (.save context)
+  (.translate context x y)
+  (set! (. context -lineWidth) 2)
+  (let [gradient (.createLinearGradient context 0 0 25 25)]
+    (.addColorStop gradient 0 "#ff0000")
+    (.addColorStop gradient 1 "#000000")
+    (set! (. context -fillStyle) gradient))
+  (.beginPath context)
+  (.arc context 0 0 25 0 (* Math/PI 2) true)
+  (.closePath context)
+  (.fill context)
+  (.stroke context)
+  (.restore context))
+
+(doseq [[x y] (balls)] (drawBall x y))
