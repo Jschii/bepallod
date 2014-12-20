cea60c8aa1bf6dd56aaaf96393acc66121a20f9e
Jussi Martikainen <jmmartik@gmail.com>
2014-12-20 12:58:44 +0200

    Draw balls with random colors

--- src/bepallod/core.cljs
+++ src/bepallod/core.cljs
@@ -5,24 +5,26 @@
 (def canvas (.getElementById js/document "canvas"))
 (def context (.getContext canvas "2d"))
 
+(def colors ["#ffff00" "#ff00ff" "#00ffff" "#ff7f7f" "#7fff7f" "#7f7fff" "#aaaaaa"])
+
 (defn balls []
   (for [x (range 25 425 50)
         y (range 25 425 50)]
-    [x y]))
+    [x y 25 (rand-nth colors)]))
 
-(defn drawBall [x y]
+(defn drawBall [x y d c]
   (.save context)
   (.translate context x y)
   (set! (. context -lineWidth) 2)
-  (let [gradient (.createLinearGradient context 0 0 25 25)]
-    (.addColorStop gradient 0 "#ff0000")
+  (let [gradient (.createLinearGradient context 0 0 d d)]
+    (.addColorStop gradient 0 c)
     (.addColorStop gradient 1 "#000000")
     (set! (. context -fillStyle) gradient))
   (.beginPath context)
-  (.arc context 0 0 25 0 (* Math/PI 2) true)
+  (.arc context 0 0 d 0 (* Math/PI 2) true)
   (.closePath context)
   (.fill context)
   (.stroke context)
   (.restore context))
 
-(doseq [[x y] (balls)] (drawBall x y))
+(doseq [[x y d c] (balls)] (drawBall x y d c))
