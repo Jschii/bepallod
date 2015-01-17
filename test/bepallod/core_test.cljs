d60222ab3556b3a753e44da9d97b102ab3e76142
Jussi Martikainen <jmmartik@gmail.com>
2015-01-17 09:36:07 +0200

    Add tests for finding matches

--- test/bepallod/core_test.cljs
+++ test/bepallod/core_test.cljs
@@ -18,3 +18,16 @@
     (is (b/is-near? (- 79) (- 40)))
     (is (not (b/is-near? (- 40) (- 80))))
     (is (not (b/is-near? (- 80) (- 40))))))
+
+(deftest find-matches-test
+  (testing "find-matches return true if sequence contains three or more consecutive elements"
+
+    (is (b/find-matches [[1 1 1 2 2 3 3 4]]))
+    (is (b/find-matches [[1 2 2 2 3 3 4 4]]))
+    (is (b/find-matches [[1 1 2 2 2 3 3 4]]))
+    (is (b/find-matches [[1 1 2 3 3 3 4 4]]))
+    (is (b/find-matches [[1 1 2 2 3 3 3 4]]))
+    (is (b/find-matches [[1 1 2 2 3 4 4 4]]))
+    (is (not (b/find-matches [[1 1 2 2 1 1 2 2]])))
+    (is (not (b/find-matches [[1 1 2 3 3 2 1 1]])))
+    (is (b/find-matches [[1 1 2 2 1 1 2 2] [1 1 1 2 2 3 3 4]]))))
